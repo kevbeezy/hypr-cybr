16be1555a43793234b87bb4c0a8ec02f0a2ab5fe
kevbeezy <kevbeezy3@gmail.com>
2026-06-08 21:08:19 -0700

    Added hyprpicker & bash-language-server installation - https://archlinux.org/packages/extra/x86_64/hyprpicker/ - https://archlinux.org/packages/extra/any/bash-language-server/

---
+++ assets/install_bash-language-server.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yay -S bash-language-server --needed --noconfirm --answerclean All --answerdiff None
---
+++ assets/install_hyprpicker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yay -S hyprpicker --needed --noconfirm --answerclean All --answerdiff None
